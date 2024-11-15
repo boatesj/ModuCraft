6762e38faf37c3674f53ce2f0bb276084fda092f
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2024-11-15 12:12:41 +0000

    Created requirements.txt

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,9 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
-import env  # Import env.py to load environment variables
+# import os
+# import env  # Import env.py to load environment variables
+# from pathlib import Path
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +82,18 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DB_URL'))
 }
+
+print('db', os.environ.get('DB_URL'))
 
 
 # Password validation
